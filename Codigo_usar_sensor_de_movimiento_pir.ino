50aa80877b090dd4b0c8267a819461af9ae5e65e
Fco. Javier Morejon <zacaruca@gmail.com>
2016-08-26 17:40:24 +0200

    Add files via upload

---
+++ Codigo_usar_sensor_de_movimiento_pir.ino
@@ -0,0 +1,89 @@
+#include <Time.h>
+#include <TimeAlarms.h>
+
+const byte RELE1Pin = 3;        // pin para el LED
+const byte RELE2Pin = 4;        // pin para el LED
+const byte RELE3Pin = 5;        // pin para el LED
+const byte RELE4Pin = 6;        // pin para el LED
+const byte PIRPin = 2;         // pin de entrada (for PIR sensor)
+
+int pirState = LOW;           // de inicio no hay movimiento
+int val = 0;                  // estado del pin
+
+
+int id_timer_pir = 0; // id del timer para activarlo o desactivarlo
+int id_timer_estado_pir = 0; // id para ver el estado del pir
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 5 * 1000;           // interval at which to blink (milliseconds)
+
+
+
+void setup()
+{
+  pinMode(RELE1Pin, OUTPUT);
+  pinMode(RELE2Pin, OUTPUT);
+  pinMode(RELE3Pin, OUTPUT);
+  pinMode(RELE4Pin, OUTPUT);
+  pinMode(PIRPin, INPUT);
+  Serial.begin(9600);
+  //setTime(8, 29, 0, 1, 1, 11); // set time to Saturday 8:29:00am Jan 1 2011
+  //creamos los timer y los deshablitmaos
+  //id_timer_pir = Alarm.timerRepeat(10 * 60, Repeats);          // timer for every 10 minutos
+  id_timer_estado_pir = Alarm.timerRepeat(6, Repeats1);            // timer for every 6 segundos
+  //Alarm.disable(id_timer_pir);
+  //Alarm.disable(id_timer_estado_pir);
+  Serial.print("Creado Timer  del pir con id");
+  Serial.println(id_timer_pir);
+  Serial.print("Creado Timer  del estado del pir con id");
+  Serial.println(id_timer_estado_pir);
+}
+
+void loop()
+{
+  val = digitalRead(PIRPin);
+
+  if (val == HIGH)   //si está activado
+  {
+    if (pirState == LOW)  //si previamente estaba apagado
+    {
+      salidas_reles(LOW);
+      Serial.println("Sensor activado");
+      Serial.println("Activando Timer");
+      //Alarm.enable(id_timer_pir);// timer for every 10 minutos
+      //Alarm.enable(id_timer_estado_pir); // timer for every 6 segundos
+      Serial.print("Activado Timer  del pir con id");
+      Serial.println(id_timer_pir);
+      Serial.print("Activado Timer  del estado del pir con id");
+      Serial.println(id_timer_estado_pir);
+      pirState = HIGH;
+    }
+  }
+  else   //si esta desactivado
+  {
+
+
+    if (pirState == HIGH)  //si previamente estaba encendido
+    {
+      salidas_reles(HIGH);
+      Serial.println("Sensor parado");
+      // Alarm.disable(id_timer_pir);
+      //Alarm.disable(id_timer_estado_pir);
+      pirState = LOW;
+    }
+  }
+}
+void salidas_reles(int estado)
+{
+  digitalWrite(RELE1Pin, estado);  //LED ON
+  digitalWrite(RELE2Pin, estado);  //LED ON
+  digitalWrite(RELE3Pin, estado);  //LED ON
+  digitalWrite(RELE4Pin, estado);  //LED ON
+}
+void Repeats1() {
+  Serial.println("5 second timer");
+  val = digitalRead(PIRPin);
+  Serial.print("Valor del PIR ")    ;
+  Serial.println(val)    ;
+}
---
+++ timer_pir.ino
@@ -0,0 +1,102 @@
+/*
+ * Programa para el cuartito de Papa
+ * 
+ * Tiempo de 10 minutos sin movimiento
+ * se apaga todo, si se mueve se enciende de nuevo
+ * 
+ * Zacaruca Software 25/08/2016
+ * 
+ * Version 1 Estable
+ */
+#include <TimeLib.h>
+#include <TimeAlarms.h>
+
+// Declaracion de variables
+const byte RELE1Pin = 3;        // pin para el LED
+const byte RELE2Pin = 4;        // pin para el LED
+const byte RELE3Pin = 5;        // pin para el LED
+const byte RELE4Pin = 6;        // pin para el LED
+const byte PIRPin = 2;          // pin de entrada (for PIR sensor)
+bool Estado_PIR_var = false;
+
+AlarmId id_timer_estado_pir;
+AlarmId id_timer_luz;
+
+void setup() {
+  // tipo de pin output o input
+  pinMode(RELE1Pin, OUTPUT);
+  pinMode(RELE2Pin, OUTPUT);
+  pinMode(RELE3Pin, OUTPUT);
+  pinMode(RELE4Pin, OUTPUT);
+  pinMode(PIRPin, INPUT);
+
+
+  Serial.begin(9600);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+    
+  // create timers, to trigger relative to when they're created
+  id_timer_estado_pir = Alarm.timerRepeat(6, Estado_PIR_leer);           // timer for every 6 seconds
+  id_timer_luz = Alarm.timerRepeat(10 * 60, Apaga_Luz);       // timer for every 10 minutos
+  Alarm.disable(id_timer_estado_pir);
+  Alarm.disable(id_timer_luz);
+  //apagamos los reles
+  salidas_reles(HIGH);
+}
+
+
+void loop() {
+  if  (Estado_PIR() == 1 && Estado_PIR_var == false) {
+    Serial.print("PIR activado y variable Estado_PIR_var = ");
+    Serial.println(Estado_PIR_var);
+    //activamos los reles
+    Serial.println("Activamos reles");
+    salidas_reles(LOW);
+    //la ponemos a true para que entre en el aranque y solo entre cuando el timer de apagado la establezca a false
+    Estado_PIR_var = true;
+    Serial.println("Activamos alarmas");
+    Alarm.enable(id_timer_estado_pir);
+    Alarm.enable(id_timer_luz);
+  }
+  Alarm.delay(1000); // wait one second between clock display
+}
+
+void Estado_PIR_leer() {
+  Serial.println("TIMER PIR --->  Leyendo estado del PIR");
+  if (Estado_PIR() == 1) {
+
+    Serial.println("PIR     M O T I O N");
+    Estado_PIR_var = true;
+    Alarm.disable(id_timer_luz);
+    delay(250);
+    Alarm.enable(id_timer_luz);
+  } else  {
+    Serial.println("PIR    N  O      MOTION");
+  }
+
+}
+
+void Apaga_Luz() {
+  Serial.println("TIMER APAGADO --->  Se apago todo");
+  //apagamos los reles
+  salidas_reles(HIGH);
+  //marcamos pir en false para iniciar de nuevo
+  Estado_PIR_var = false;
+  //disable los timer para empezar de nuevo
+  Alarm.disable(id_timer_estado_pir);
+  Alarm.disable(id_timer_luz);
+}
+bool Estado_PIR() {
+  if (digitalRead(PIRPin) == 1 ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void salidas_reles(int estado)
+{
+  digitalWrite(RELE1Pin, estado);  //LED ON
+  digitalWrite(RELE2Pin, estado);  //LED ON
+  digitalWrite(RELE3Pin, estado);  //LED ON
+  digitalWrite(RELE4Pin, estado);  //LED ON
+}
